cc017baca5d6dbab8391a88e9ef9eb98b129d4ab
mally514 <jaymarbajala110@gmail.com>
2019-11-27 17:14:28 +0800

    start challenging 1 and with back-end

---
+++ challenges/flask/flask-qoutes-v1.0-challenge1-with-backend/src/app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template,request, redirect, session, flash
+
+from mysqlconnection import connectToMySQL 
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route("/")
+def index():
+	
+	return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+	errors = {}
+	if request.method == "POST":
+		try:
+			if  len(request.form['name']) < 5:
+				flash("Name should be at least 5 characters")
+			if  len(request.form['username']) < 5:
+				flash("Username should be at least 5 characters")
+			if len(request.form['password']) < 8:
+				flash("Password should be at least 8 characters")
+			if request.form['password'] != request.form['c_password']:
+				flash("Passwords do not match")
+		except Exception as e:
+				flash("Unknown error")
+		if '_flashes' in session.keys():
+			return redirect('/')
+		else:
+			mysql = connectToMySQL()
+			query = "INSERT INTO users (name, username, password,created_at) VALUES (%(name)s, %(username)s, %(password)s,NOW());"
+			data = {
+				"name": request.form['name'],
+				"username": request.form['username'],
+				"password": request.form['password'],
+			}
+			user = mysql.query_db(query, data)
+
+			session['is_logged_in'] = True
+			session['name'] = request.form['name']
+			session['user_id'] = user
+
+			return redirect("/dashboard")
+
+
+@app.route("/dashboard", methods = ["GET"])
+def dashboard():
+	if 'is_logged_in' in session:
+		if session['is_logged_in'] == True:
+			try:
+				mysql = connectToMySQL()
+				query = "SELECT * FROM users WHERE id = %(id)s LIMIT 1;"
+				data = {
+					"id": session['user_id']
+				}
+
+				user = mysql.query_db(query, data)
+			except Exception as e:
+				flash("Invalid session")
+				return redirect("/")
+			return render_template("dashboard.html", user = user)
+		else:
+			flash("User is not logged in")
+			return redirect("/")
+	else:
+		flash("User is not logged in")
+		return redirect("/")
+
+@app.route('/logout', methods=['GET'])
+def logout():
+	request.session.flush()
+	return redirect("/")
+
+@app.route('/reset', methods=['GET'])
+def reset():
+	mysql = connectToMySQL()
+	mysql.query_db("SET FOREIGN_KEY_CHECKS = 0;")
+	mysql.query_db("DELETE FROM users WHERE email in('mally5@yahoo.com', 'brian@gmail.com', 'james@gmail.com');")
+	mysql.query_db("SET FOREIGN_KEY_CHECKS = 1;")
+
+	return redirect('/')
+
+if __name__ == "__main__":
+	app.run(port=8000, debug=True)
