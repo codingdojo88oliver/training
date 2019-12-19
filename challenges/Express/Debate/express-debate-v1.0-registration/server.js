f67b53a3cd3997bbed52d75d35a85a45ac88d2f8
mally514 <jaymarbajala110@gmail.com>
2019-12-19 16:58:58 +0800

    Added Express Debate

---
+++ challenges/Express/Debate/express-debate-v1.0-registration/server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser')
+const mysql = require('mysql');  
+const session = require('express-session')
+const moment = require('moment');
+const flash = require('express-flash');
+
+var config = {  
+    host: "localhost",  
+    user: "root",  
+    password: "",  
+    database: "debate" 
+}
+
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(config); 
+
+    connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            console.log('reconnecting...');
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
+app.use(flash());
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {}
+}))
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + "/public"));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(request, response) {
+    
+     response.render('index');
+});
+
+app.post('/register', function(req, res) {
+    res.send("Hello, World! Make sure you remove this line of code from views.py file and replace it with your own code.")
+     // Your code goes here...
+});
+
+
+app.get('/dashboard', function(req, res) {
+    res.send("Hello, World! Make sure you remove this line of code from views.py file and replace it with your own code.")
+     // Your code goes here...
+});
+
+app.get('/logout', function(req, res) {
+    req.session.destroy();
+    res.redirect("/");
+});
+
+app.get("/reset", function(req, res){
+    connection.query("DELETE FROM users WHERE email IN ('brian@gmail.com', 'mally@gmail.com');", function(){
+        res.redirect("/")
+    });
+})
+
+
+app.listen(8000, function(){
+    console.log('Your node js server is running on PORT 8000');
+});
